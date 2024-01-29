fcde9975cdb0f0f6f3b148bf5299dec48347c675
Lottar1665 <rilottar5221@gmail.com>
2024-01-29 16:17:59 -0300

    update 1.1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,49 @@
 import React from 'react'
 import styles from '../styles/Home.module.scss'
+import Image from "next/image"
+
+import ImageLogo from "../pages/public/img/Logo.svg"
+import ImageFundo from "../pages/public/fundo.png"
+
 
 function index() {
   return (
     <>
-    <main  className='w-auto h-auto'>
-      <div className='items-center text-5xl 
-      justify-center flex font-serif text-white'>
-        <text>Login</text>
-      </div>
-      <div className='flex items-center justify-center mt-2 text-white'>
-          <input type="email" className='mt-5 w-80 h-14 rounded-xl bg-input'/>
-      </div>
-      
-      <div className='flex items-center justify-center mt-2'>
-            <input type="password" className='w-80 h-14 rounded-xl bg-input'/>
+    
+    <main  className='flex'>
+    <div className='xl:ml-96 xl:mr-96'>
+    <div className='flex items-center justify-center'>
+    <Image src={ImageLogo} alt={''} className='w-48 h-48 md:w-64 md:h-64' />
+    </div>
+      <div 
+        className='items-center 
+        text-3xl 
+        md:text-5xl
+        justify-center 
+        flex 
+        font-serif 
+      text-white
+        mb-1
+        mt-5
+        '>
+            <text>Login</text>
       </div>
 
+      <div className='flex items-center justify-center'>
+      <div className='flex items-center justify-center mt-2 rounded-xl bg-input w-80 md:w-96'>
+          <input type="email" placeholder='Digite seu email' className='w-64 h-14 rounded-xl bg-input'/>
+      </div>
+      </div>
+      
+      <div className='flex items-center justify-center'>
+      <div className='flex items-center justify-center mt-2 rounded-xl bg-input w-80 md:w-96'>
+            <input type="password" placeholder='Digite sua senha' className='w-64 h-14 rounded-xl bg-input'/>
+      </div>
+      </div>
+      
+
       <div className='flex justify-center items-center'>
-        <button className='mt-5 border-black font-serif bg-bg-enter text-enter w-80 h-16 rounded-xl '>Entrar</button>
+        <button className='mt-5 border-black font-serif bg-bg-enter text-enter w-80 h-14 rounded-xl md:w-96'>Entrar</button>
       </div>
         
         <div className='flex justify-center items-center'>
@@ -28,6 +53,11 @@ function index() {
         <div className='flex justify-center items-center'>
           <button className='text-bg-enter mt-20 font-serif'>Criar conta</button>
         </div>
+      </div>
+      <div className='flex w-screen h-screen'>
+      <Image src={ImageFundo} alt={''}/>
+      </div>
+    
     </main>
     </>
     
