38a467b574543d751520bf8082b9d19113760dc1
Lottar1665 <71538144+Lottar1665@users.noreply.github.com>
2024-01-31 08:10:38 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,14 +38,14 @@ function index() {
         <MdOutlineEmail className='w-[20] h-[20] bg-input text-icon ml-[16px] mr-[16px]' />
           <input type="email" 
           placeholder='Digite seu email' 
-          className='w-80 h-14 rounded-xl bg-input '/>
+          className='w-80 h-14 rounded-xl bg-input focus-within:border-bg-enter'/>
       </div>
       </div>
       
       <div className='flex items-center justify-center'>
       <div className='flex items-center justify-center mt-2 rounded-xl bg-input w-96'>
       <RiLockPasswordFill className='w-[20] h-[20] bg-input text-icon ml-[16px] mr-[16px]'/>
-            <input type="password" placeholder='Digite sua senha' className='w-80 h-14 rounded-xl bg-input '/>
+            <input type="password" placeholder='Digite sua senha' className='w-80 h-14 rounded-xl bg-input focus-within:border-bg-enter'/>
       </div>
       </div>
       
