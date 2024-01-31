8b5d9f61cd03102553e16a20097c70b7a6a443d6
Lottar1665 <71538144+Lottar1665@users.noreply.github.com>
2024-01-31 08:23:03 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,9 +44,8 @@ function index() {
       
       <div className='flex items-center justify-center'>
       <div className='flex items-center justify-center mt-2 rounded-xl bg-input w-96'>
-      <RiLockPasswordFill className='w-[20] h-[20] bg-input text-icon ml-[16px] mr-[16px]'>
-        <input type="password" placeholder='Digite sua senha' className='w-80 h-14 rounded-xl bg-input border border-bg-enter placeholder-bg-enter focus:outline-none focus:ring-bg-enter focus:ring-1'/>
-      </RiLockPasswordFill>
+      <RiLockPasswordFill className='w-[20] h-[20] bg-input text-icon ml-[16px] mr-[16px]'/>
+        <input type="password" placeholder='Digite sua senha' className='w-80 h-14 rounded-xl bg-input border border-bg-enter placeholder-bg-enter focus-within:outline-none focus-within:ring-bg-enter focus-within:ring-1'/>
             
       </div>
       </div>
